4d5e7a0fb4a313fc9c37dd995754dbe24a736565
mira <ngohoanganh@MacBook-Pro-cua-Ngo.local>
2026-01-12 23:17:48 +0700

    init app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,35 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      // 4 spaces indent
+      "indent": ["error", 4, {
+        "SwitchCase": 1,
+        "ignoredNodes": ["ConditionalExpression"]
+      }],
+    }
+  },
+
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+
+    "node_modules/**",
+    "*.config.js",
+    "*.config.mjs",
+    "dist/**",
+    "coverage/**"
   ]),
 ]);
 
